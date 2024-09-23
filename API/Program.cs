faf494725a90480d34fb2a5adf1df8842593d288
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2024-09-23 14:36:45 +0300

    Adding sqlite database & editing project structure

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext> (x =>
+    x.UseSqlite(builder.Configuration.GetConnectionString("constr")));
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
